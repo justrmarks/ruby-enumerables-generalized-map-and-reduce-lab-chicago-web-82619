3ef77e3e2a90705de14e8d4a00079492ace77043
Justice Marks <her@shedaddy.com>
2019-07-24 20:45:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
 # Your Code Here
+
+def map(source_array)  
+  result = []
+  i=0
+  
+  while i<source_array.length do
+    result.push(yield(source_array[i]))
+    i+= 1
+  end
+  return result
+end
+
+def reduce(source_array, starting_value = )
+result = starting_value
+i=0
+
+while i<source_array.length do
+    result = (yield(result,source_array[i]))
+    i+= 1
+  end
+return result
+end
